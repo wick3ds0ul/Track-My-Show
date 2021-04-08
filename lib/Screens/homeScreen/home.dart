132b848e649ef06e7eaa9e3de311d57bd1a6ea8d
deepak <deepakpawade.97@gmail.com>
2021-04-08 19:17:15 +0530

    Close to polished

--- lib/Screens/homeScreen/home.dart
+++ lib/Screens/homeScreen/home.dart
@@ -46,31 +46,7 @@ class _HomeScreenState extends State<HomeScreen> {
             context: context,
             builder: (context) {
               return ExitModlal();
-            }
-            // builder: (context) => new AlertDialog(
-            //   title: new Text('Confirm Exit?',
-            //       style: new TextStyle(
-            //           color: Colors.black,
-            //           fontSize: 20.0,
-            //           fontFamily: 'Comfortaa')),
-            //   content: new Text(
-            //       'Are you sure you want to exit the app? Tap \'Yes\' to exit \'No\' to cancel.'),
-            //   actions: <Widget>[
-            //     new FlatButton(
-            //       onPressed: () {
-            //         // this line exits the app.
-            //         SystemChannels.platform.invokeMethod('SystemNavigator.pop');
-            //       },
-            //       child: new Text('Yes', style: new TextStyle(fontSize: 18.0)),
-            //     ),
-            //     new FlatButton(
-            //       onPressed: () =>
-            //           Navigator.pop(context), // this line dismisses the dialog
-            //       child: new Text('No', style: new TextStyle(fontSize: 18.0)),
-            //     )
-            //   ],
-            // ),
-            ) ??
+            }) ??
         false;
   }
 }
